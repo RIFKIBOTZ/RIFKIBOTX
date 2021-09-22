11b789a120320a4480ed7635734152b6ffdd3a36
RIFKI BOTX <88698204+RIFKIBOTZ@users.noreply.github.com>
2021-09-23 06:50:06 +0700

    BY RIFKI BOTZ

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
-z="
-";xCz='plea';ICz='pen ';bBz='si T';EBz='all';FBz='audi';xBz=' "> ';eBz='i (I';Az='pkg ';RCz='etai';Tz='bp -';kBz='to r';fBz='n or';Wz='tess';pz='t-oc';Jz='sox ';KBz='cowt';dBz='x-Ap';Bz='inst';gBz='der ';iCz='pi S';Lz='jq -';BBz='aw=t';Zz=' -O ';vz='er/i';KCz='s://';oBz=' ple';nz='hub.';HBz='x';lCz='Klik';QCz='ps/d';KDz='h';GCz='er';wCz='on, ';Yz='t -y';CCz='tall';RBz=' Ber';Mz='figl';ECz='read';Xz='erac';jBz='bot ';gz='ind.';sBz='pi a';BDz='er t';yCz='se c';Pz=' -y';fCz='ll A';qz='r/te';TBz='n La';tCz=' hav';pBz='ase ';OCz='stor';XBz='n In';DBz='npm ';ADz='lick';ABz='ta?r';tz='lob/';ez='essd';DDz='n th';BCz=' ins';cz='/sha';LBz='hink';tBz='ppli';bCz=' Sud';PCz='e/ap';oCz='ntuk';rBz='ux-A';qCz='nkan';SCz='ls?i';Iz='ay -';WBz='ahka';JDz='rt.s';VCz='rmux';JBz='r';WCz='.api';rz='ssda';YCz='p 3s';IDz='bash';hBz='for ';jCz='ilah';Dz='term';xz='rain';kCz='kan ';vBz='on)"';sCz=' you';aCz='Jika';Uz='node';hz='trai';cBz='ermu';MCz='.goo';Hz='cows';lBz='un s';UBz='ncar';aBz='lika';Rz='eg l';HDz='rt"';QBz=' Bot';Kz='-y';UCz='m.te';FDz='t)"';mCz=' Ent';iBz='the ';Fz='pi -';GDz=' sta';VBz=' Sil';iz='nedd';uBz='cati';uz='mast';SBz='jala';OBz='on "';az='~/..';Sz='ibwe';Vz='js -';hCz='asi ';IBz='clea';sz='ta/b';DCz='"';ACz='r to';wz='nd.t';TCz='d=co';wBz='echo';EDz='e bo';PBz='Agar';Oz='wget';nBz='hly,';Cz='all ';JCz='http';MBz=' -f ';uCz='e in';jz='ata ';dz='re/t';fz='ata/';cCz='ah M';yz='edda';oz='com/';nCz='er U';Gz='y';NBz='daem';ZCz='drag';CDz='o ru';CBz='rue"';ZBz='l Ap';Nz='et -';Qz='ffmp';mz='/git';GBz='t fi';kz='"htt';FCz=' ent';Ez='ux-a';bz='/usr';yBz='ente';vCz='led ';XCz='slee';YBz='stal';HCz='ux-o';qBz='Term';rCz=' (If';dCz='engi';lz='ps:/';LCz='play';eCz='nsta';NCz='gle.';pCz=' Men';gCz='plik';mBz='moot';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Az$Bz$Cz$Hz$Iz$Gz$z$Az$Bz$Cz$Jz$Kz$z$Az$Bz$Cz$Lz$Gz$z$Az$Bz$Cz$Mz$Nz$Gz$z$Az$Bz$Cz$Oz$Pz$z$Az$Bz$Cz$Qz$Rz$Sz$Tz$Gz$z$Az$Bz$Cz$Uz$Vz$Gz$z$Az$Bz$Cz$Wz$Xz$Yz$z$Oz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$Wz$Xz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$BBz$CBz$z$DBz$Bz$EBz$z$DBz$FBz$GBz$HBz$z$IBz$JBz$z$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$WBz$XBz$YBz$ZBz$aBz$bBz$cBz$dBz$eBz$fBz$gBz$hBz$iBz$jBz$kBz$lBz$mBz$nBz$oBz$pBz$Bz$Cz$iBz$qBz$rBz$sBz$tBz$uBz$vBz$z$wBz$xBz$yBz$ACz$BCz$CCz$DCz$z$ECz$FCz$GCz$z$Dz$HCz$ICz$JCz$KCz$LCz$MCz$NCz$oz$OCz$PCz$QCz$RCz$SCz$TCz$UCz$VCz$WCz$z$IBz$JBz$z$XCz$YCz$z$KBz$LBz$MBz$ZCz$OBz$aCz$bCz$cCz$dCz$eCz$fCz$gCz$hCz$qBz$rBz$iCz$jCz$kCz$lCz$mCz$nCz$oCz$pCz$SBz$qCz$QBz$rCz$sCz$tCz$uCz$YBz$vCz$iBz$qBz$rBz$sBz$tBz$uBz$wCz$xCz$yCz$ADz$mCz$BDz$CDz$DDz$EDz$FDz$z$wBz$xBz$yBz$ACz$GDz$HDz$z$ECz$FCz$GCz$z$IBz$JBz$z$IDz$GDz$JDz$KDz"
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+
+echo "DONE INSTALLING ALL PACKAGE REQUIRE, TINGGAL RUN npm start"
